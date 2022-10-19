9e8f00ba606e1b81ea21762b26922cfd914024b5
artepatrick <45848477+artepatrick@users.noreply.github.com>
2022-10-19 08:33:42 -0300

    mais uma aula do Alexandre - multiplicando elementos

--- organo/src/App.js
+++ organo/src/App.js
@@ -2,21 +2,26 @@ import Banner from './componentes/Banner';
 import Formulário from './componentes/Formulário';
 import React from 'react'
 import Box from './componentes/Box';
+import './index.css'
 
 function App(props) {
   console.log(props);
   return (
-    <body>
-      <div className="App">
-        <Banner />
+    <div>
+      <Banner />
+      <div className="boxFormulário">
         <Formulário />
-        <header className="App-header">
-        </header>
       </div>
-      <section>
-        <Box nomePessoa="Patrick" profissao='Product Manager' />
-      </section>
-    </body>
+      <div className='faixaBoxes'>
+        <h2>Desenvolvimento</h2>
+        <section>
+          <Box nomePessoa="Laura" profissao='Quality Assurance' imagemPerfil='https://img.elo7.com.br/product/original/3EECB71/desenho-personalizado-para-usar-de-foto-de-perfil-e-etc-personalizado.jpg' />
+          <Box nomePessoa="Denise" profissao='Product Owner' imagemPerfil='https://img.elo7.com.br/product/main/3E26D20/desenho-personalizado-para-perfil-desenho-personalizado.jpg' />
+          <Box nomePessoa="Patrick" profissao='Product Manager' imagemPerfil='https://img.freepik.com/vetores-premium/imagem-de-perfil-de-personagem-de-desenho-animado-avatar-jovem_18591-55058.jpg?w=2000' />
+          <Box nomePessoa="Alexandre" profissao='Desenvolvedor' imagemPerfil='https://img.freepik.com/vetores-premium/imagem-de-perfil-de-personagem-de-desenho-animado-avatar-jovem_18591-55054.jpg?w=2000' />
+        </section>
+      </div>
+    </div>
   );
 }
 export default App;
--- organo/src/componentes/Box/index.js
+++ organo/src/componentes/Box/index.js
@@ -1,23 +1,23 @@
 import './Box.css'
 import React from 'react'
+import fotoPerfil from './perfil-baixa.jpg'
 
-const dadosPessoas = [
-    { 'nomePessoa': 'Alexandre Krabbe', 'cargoPessoa': 'Desenvolvedor', 'foto': 'https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg' }
-]
+// const dadosPessoas = [
+//     { 'nomePessoa': 'Alexandre Krabbe', 'cargoPessoa': 'Desenvolvedor', 'foto': 'https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg' }
+// ]
 
 const Box = (props) => {
     return (
         <div className='bigBox'>
-            <div className='faixaBox' />
-            <div>
-
-            </div>
-            <div>
+            <div className='faixaBox'></div>
+            <section className='sectionTexto'>
+                <img className='foto-redonda' src={props.imagemPerfil} />
                 <h3>{props.nomePessoa}</h3>
-                <p>{props.profissão}</p>
-            </div>
+                <p>{props.profissao}</p>
+            </section>
         </div>
     )
 }
 
 export default Box
+
